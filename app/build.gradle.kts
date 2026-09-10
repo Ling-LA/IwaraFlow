db20b391188598472619754e207273aa037a27f3
Ling-LA <93372735+Ling-LA@users.noreply.github.com>
2026-09-10 09:54:22 +0800

    chore: bump IwaraFlow to v0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ling.iwaraflow"
         minSdk = 28
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.7.0"
+        versionCode = 17
+        versionName = "0.7.1"
     }
 
     buildTypes {
